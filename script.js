2d04b1a2d4269c9c314a8f1a17e9bebe0af85be4
Leonel Onnembo <onnembol@gmail.com>
2023-10-28 19:43:39 -0300

    qr

--- script.js
+++ script.js
@@ -5,25 +5,48 @@ document.addEventListener("DOMContentLoaded", function() {
     const downloadButton = document.getElementById("download");
 
     generateButton.addEventListener("click", function() {
-        const inputs = document.querySelectorAll(".qr-input");
-        const data = Array.from(inputs)
-            .map(input => input.value)
-            .join(';');
-
-        const qr = new QRious({
-            element: qrCodeCanvas,
-            value: data,
-            size: 200
-        });
-
-        // Habilitar el botón de descarga
-        downloadButton.style.display = "block";
-        downloadButton.addEventListener("click", function() {
-            const qrDataURL = qr.toDataURL();
-            const link = document.createElement("a");
-            link.href = qrDataURL;
-            link.download = "codigo-qr.png";
-            link.click();
-        });
+        generateQRCode();
     });
+
+    // Detectar la tecla "Enter" en el campo de entrada
+    inputContainer.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            generateQRCode();
+        }
+    });
+
+    function generateQRCode() {
+        const inputs = document.querySelectorAll(".qr-input");
+        const hasContent = Array.from(inputs).some(input => input.value.trim() !== '');
+
+        if (hasContent) {
+            const data = Array.from(inputs)
+                .map(input => input.value)
+                .join(';');
+
+            const qr = new QRious({
+                element: qrCodeCanvas,
+                value: data,
+                size: 200
+            });
+
+            // Habilitar el botón de descarga
+            downloadButton.style.display = "block";
+            downloadButton.addEventListener("click", function() {
+                const qrDataURL = qr.toDataURL();
+                const link = document.createElement("a");
+                link.href = qrDataURL;
+                link.download = "codigo-qr.png";
+                link.click();
+            });
+        } else {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Debes introducir un texto o enlace.',
+                showConfirmButton: false,
+                timer: 1600
+              });
+        }
+    }
 });
